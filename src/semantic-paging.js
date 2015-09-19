1ba06d14203930085c0ad9e8a378714ebef17438
bmaniteja <maniteja4@gmail.com>
2015-09-20 00:59:55 +0530

    Update and rename paging.js to semantic-paging.js
    Changing template to use Semantic UI Pagination menu

--- src/semantic-paging.js
+++ src/semantic-paging.js
@@ -42,15 +42,15 @@ angular.module('bw.paging', []).directive('paging', function () {
 
         // Assign the angular directive template HTML
         template: 
-            '<ul ng-hide="Hide" ng-class="ulClass"> ' +
-                '<li ' +
+            '<div ng-hide="Hide" ng-class="ulClass" class="ui pagination menu">'+
+                     '<div  class="item"' +
                     'title="{{Item.title}}" ' +
                     'ng-class="Item.liClass" ' +
                     'ng-click="Item.action()" ' +
                     'ng-repeat="Item in List"> ' +
                         '<span ng-bind="Item.value"></span> ' +
-                '</li>' +
-            '</ul>'
+                '</div>' +
+             '</div>'
     };
     
     
